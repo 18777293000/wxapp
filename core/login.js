7d734ade866f8f5b76e2d794a2cce03e326d79f4
18777293000 <120953990@qq.com>
2020-01-10 10:51:16 +0800

    删除了授权弹窗里的1

--- core/login.js
+++ core/login.js
@@ -1,7 +1,7 @@
 module.exports = function(e) {
     var t = this, r = t.page.currentPage;
     t.page.currentPageOptions;
-    r && "pages/index/index" === r.route && "my" === t.platform || this.request({
+    r && "/pages/index/index" === r.route && "my" === t.platform || this.request({
         url: this.api.share.index,
         success: function(e) {
             0 == e.code && (t.page.setPhone(), t.trigger.run(t.trigger.events.login));
