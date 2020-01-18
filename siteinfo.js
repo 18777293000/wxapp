99d174ffca29e55aa0cd97a059e92e2476cf5be9
18777293000 <18777293000@163.com>
2020-01-18 18:02:53 +0800

    修改分类显示

--- siteinfo.js
+++ siteinfo.js
@@ -1,9 +1,9 @@
 module.exports = {
     name: "唐记小店",
-    uniacid: "2",
-    acid: "2",
+    uniacid: "16",
+    acid: "16",
     multiid: "0",
     version: "1.0",
-    siteroot: "https://nnxc.wewaka.com/app/index.php",
+    siteroot: "https://wakakago.510show.com/app/index.php",
     design_method: "3"
 };
